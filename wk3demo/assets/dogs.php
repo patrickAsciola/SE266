458e82fd2fdea9768b94b808db2a53c402f21435
Patrick Asciola <paasciola@email.neit.edu>
2017-10-16 12:53:22 -0400

    Table of dogs

--- wk3demo/assets/dogs.php
+++ wk3demo/assets/dogs.php
@@ -6,10 +6,28 @@
  * Time: 12:15 PM
  */
 function getDogsAsTable($db) {
-    $sql = $db->prepare("SELECT * FROM animals");
-    $sql->execute();
-    $results = $sql->fetchAll(PDO::FETCH_ASSOC);
-    return $results;
+    try {
+        $sql = $db->prepare("SELECT * FROM animals");
+        $sql->execute();
+        $dogs = $sql->fetchAll(PDO::FETCH_ASSOC);
+        if( $sql->rowCount() > 0)
+        {
+            $table = "<table>" . PHP_EOL;
+            foreach ($dogs as $dog){
+                $table .= "<tr><td>" . $dog['name'];
+                $table.="</td><td>" . $dog['gender'];
+                $table .= "</td><td>" . $dog['fixed'];
+                $table .= "</td></tr>";
+        }
+
+            $table .="</table>";
+        }else{
+            $table = "Life is sad. There is no dogs.";
+        }
+        return $table;
+    }catch(PDOException $e){
+        die("There was a problem retrieving the dogs");
+    }
     /*if( count($results) ){
         foreach($results as $dog)
         {
--- wk3demo/index.php
+++ wk3demo/index.php
@@ -3,6 +3,6 @@ require_once("assets/dbconn.php");
 require_once("assets/dogs.php");
 include_once("assets/header.php");
 $db = dbConn();
-var_dump( getDogsAsTable($db));
+echo getDogsAsTable($db);
 include_once ("assets/footer.php");
 ?>
